eb7090d26e609d89189c316db5edd0f4a463ada2
Abolaji Bisiriyu Sulaiman <abolajibisiriyu@gmail.com>
2019-01-28 12:24:23 +0100

    scaffold for the ping model and ping class

--- lib/ping.dart
+++ lib/ping.dart
@@ -1,53 +1,48 @@
-import 'dart:_http';
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/services.dart';
-import 'package:http/http.dart';
-import 'package:ping/ping_builder.dart';
+import 'package:reflectable/reflectable.dart';
+import 'package:reflectable/mirrors.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:ping/ping_model.reflectable.dart';
+
+import 'package:ping/ping_model.dart';
+
+@pingReflectable
 class Ping {
-
-  HttpClient client;
-  PingBuilder pingBuilder;
-
-  /// =====================
-  /// GET request to return a Future
-  /// =====================
-  Future<T> getRequest<T>(String endpoint, Future<T> fn(Client client)) async {
-    var res = await http.get("${pingBuilder.getBaseUrl()} $endpoint");
-
-    if (res.statusCode == 200) {
-      var mappedResponse = res.body as T;
-      return Future.value(mappedResponse);
-    } else {
-      return Future.error(Error());
-    }
+  Ping() {
+    initializeReflectable();
   }
 
-  /// =====================
-  /// GET request with callbacks
-  /// =====================
-  Future<T> getRequestWithCallback<T>(String endpoint,
-      {Future<T> Function(T response) onSuccess,
-      Future<T> Function(T response) onFailure}) async {
-
-    var res = await http.get("${pingBuilder.getBaseUrl()} $endpoint");
-    if (res.statusCode == 200) {
-      var mappedResponse = res.body as T;
-      return await onSuccess(mappedResponse);
-    } else {
-      return Future.error(Error());
-    }
-
+  Future get(String url, {Type model}) {
+    return http.get(url).then((res) {
+      if (model != null) {
+        ClassMirror mirror = pingReflectable.reflectType(model);
+        return mirror.newInstance('fromJson', [json.decode(res.body)]);
+//        return reflectClass(model)
+//            .newInstance(Symbol('fromJson'), [json.decode(res.body)]).reflectee;
+      }
+      return PingModel.fromJson(json.decode(res.body));
+    });
   }
+}
 
-  static final Ping instance = new Ping._internal();
+@pingReflectable
+@metaReflector
+class User extends PingModel {
+  final name;
+  final email;
 
-  factory Ping() {
-    return instance;
-  }
+  @MetaCapability()
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        email = json['email'],
+        super.fromJson(json);
+}
 
-  Ping._internal();
-
+main() {
+  Ping()
+      .get('https://jsonplaceholder.typicode.com/users/1', model: User)
+      .then((val) => print(val.body['email']));
 }
